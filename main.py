99feb26d8af3313eab8ea0abd909083e8c685947
Justin <48774742+realqwertyp@users.noreply.github.com>
2022-06-13 15:03:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,24 @@
 import pygame
 import random
 
-WIDTH = 800
-HEIGHT = 600
+WIDTH, HEIGHT = 800, 600
 
 pygame.init()
 display = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Car Game')
 
 # colours
-RED = (255, 102, 102)
-BLUE = (128, 191, 255)
-YELLOW = (230, 184, 0)
-GREEN = (0, 150, 59)
-BLACK = (26, 26, 26)
-WHITE = (242, 242, 242)
-GREY = (179, 179, 179)
-
+RED, BLUE, YELLOW, GREEN, BLACK, WHITE, GREY = (255, 102, 102), (128, 191, 255), (234, 234, 2), (0, 150, 59), (26, 26, 26), (242, 242, 242), (179, 179, 179)
 color = RED
 
 # car dimensions
-carW = 40
-carL = 100
-carX = 390
-carY = 420
-obstaclesX = 20
+carW, carL, carX, carY = 40, 100, 390, 420
+
+# obstacles
+obstaclesW, obstalcesL = 90, 90
+obstaclesX = []
+obstaclesY = [] 
+obstaclesSpeed = 20
 
 # import images
 car = pygame.image.load('images/car.png').convert_alpha()
@@ -32,7 +26,11 @@ game = pygame.image.load('images/game.png').convert_alpha()
 ttt = pygame.image.load('images/2022.gif').convert_alpha()
 easyButton = pygame.image.load('images/easybutton.png').convert_alpha()
 hardButton = pygame.image.load('images/hardbutton.png').convert_alpha()
+redButton = pygame.image.load('images/red.png')
+blueButton = pygame.image.load('images/blue.png')
+greenButton = pygame.image.load('images/green.png')
 
+inMenu = True
 
 class Button():
     def __init__(self, x, y, image):
@@ -41,28 +39,15 @@ class Button():
         self.rect.topleft = (x, y)
 
     def draw(self):
+        global inMenu
         action = False
         display.blit(self.image, (self.rect.x, self.rect.y))
+
         position = pygame.mouse.get_pos()
 
         if self.rect.collidepoint(position):
             if pygame.mouse.get_pressed()[0] == 1:
-                print("d")
-
-
-easy_button = Button(100, 400, easyButton)
-hard_button = Button(500, 400, hardButton)
-
-
-def menu():
-    display.fill(BLACK)
-    display.blit(car, (20, 30))
-    display.blit(game, (250, 10))
-    display.blit(ttt, (550, 10))
-
-    easy_button.draw()
-    hard_button.draw()
-
+                inMenu = False
 
 def gameWindow():
     display.fill(BLACK)
@@ -82,12 +67,24 @@ def gameWindow():
 
     pygame.draw.rect(display, color, (carX, carY, carW, carL))  # car
 
-
-inMenu = True
 while inMenu:
-    # pygame.event.clear()
-    menu()
+    display.fill(BLACK)
+    display.blit(car, (20, 30))
+    display.blit(game, (250, 10))
+    display.blit(ttt, (550, 10))
 
+    easy_button = Button(100, 400, easyButton)
+    hard_button = Button(500, 400, hardButton)
+    red_button = Button(100, 200, redButton)
+    blue_button = Button(300, 200, blueButton)
+    green_button = Button(500, 200, greenButton)
+
+    easy_button.draw()
+    hard_button.draw()
+    red_button.draw()
+    blue_button.draw()
+    green_button.draw()
+   
     key = pygame.key.get_pressed()
     if key[pygame.K_ESCAPE]:
         pygame.quit()
@@ -95,22 +92,20 @@ while inMenu:
     for event in pygame.event.get():
 
         if event.type == pygame.QUIT:
-            inMenu = False
+            pygame.quit()
 
     pygame.display.update()
 
 inGame = True
 while inGame:
     gameWindow()
-    pygame.draw.rect(display, WHITE, (obstaclesX * 40 + 240, 0, 40, 40))
-
     key = pygame.key.get_pressed()
     if key[pygame.K_ESCAPE]:
         pygame.quit()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            inGame = False
+            pygame.quit()
 
     if (key[pygame.K_LEFT] or key[pygame.K_a]) and carX >= 261:
         carX -= 1
